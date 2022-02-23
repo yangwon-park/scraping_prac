683917ec862236eebdf14509fed1fa181725f04a
yangwon-park <97505799+yangwon-park@users.noreply.github.com>
2022-02-23 13:18:59 +0900

    04. user agent

---
+++ webscraping_basic/5_user_agent.py
@@ -0,0 +1,12 @@
+import requests
+# user agent 정보 확인
+# https://www.whatismybrowser.com/detect/what-is-my-user-agent/
+url = "http://nadocoding.tistory.com"
+headers = {"User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36"}
+res = requests.get(url, headers=headers)
+res.raise_for_status()
+
+with open("nadocomding.html", "w", encoding="utf-8") as f:
+    f.write(res.text)
+
+
